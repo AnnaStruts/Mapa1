22b995b0c135bdedc232755480e84e65ce8d79e2
AnnaStruts <93871129+AnnaStruts@users.noreply.github.com>
2021-11-09 14:58:04 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-u04qBzLw1R0WX4QNVY_R1Ir5N4nWqsI/edit#gid=816435972';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1me8yalkwc3MuLNKMZgbg38taHuC-9PA2-ba1PHOJyjE/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
